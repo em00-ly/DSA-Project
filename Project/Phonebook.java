3de48f018dc44ee500b2332ac4ca7784fa705139
em00-ly <emily25mudjanima@gmail.com>
2024-10-12 21:18:49 +0200

    Update Phonebook.java

--- Project/Phonebook.java
+++ Project/Phonebook.java
@@ -22,12 +22,11 @@ class ContactList {
     public ContactList() {
         head = null; // Initially, the list is empty
     }
-
-    // Function to add a contact to the linked list
+     // insert contact 
     public void insertContact(String name, String phoneNumber) {
-        ContactNode newContact = new ContactNode(name, phoneNumber); // Create a new contact node
-        newContact.next = head; // Point new contact to the current head
-        head = newContact; // Update head to the new contact
+        ContactNode newContact = new ContactNode(name, phoneNumber); 
+        newContact.next = head; 
+        head = newContact; 
     }
 
     // Function to delete a contact by name
