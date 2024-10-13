760f36824f60b5f3f3225538bb3aabed05663d6e
anopakaren <mukwendikarenn@gmail.com>
2024-10-13 15:37:16 +0200

    Create Display Contacts.java
    //Anopaishe Mukwendi 224035622

---
+++ Project/Display Contacts.java
@@ -0,0 +1,12 @@
+  public void displayContacts() {
+        if (head == null) {
+            System.out.println("Phonebook is empty."); 
+        } else {
+            ContactNode temp = head;
+            while (temp != null) {
+                
+                System.out.println("Name: " + temp.name + ", Phone: " + temp.phoneNumber);
+                temp = temp.next;
+            }
+        }
+    }
