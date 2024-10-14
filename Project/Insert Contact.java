91974f532a1a2572f0e428aa357bd3889a37e5b7
em00-ly <emily25mudjanima@gmail.com>
2024-10-14 16:57:26 +0200

    Update Insert Contact.java

--- Project/Insert Contact.java
+++ Project/Insert Contact.java
@@ -1,5 +1,22 @@
-  public void insertContact(String name, String phoneNumber) {
-        ContactNode newContact = new ContactNode(name, phoneNumber); 
-        newContact.next = head; 
-        head = newContact; 
-    }
+public ContactNode(String name, int phoneNumber) {
+this.name = name;
+this.phoneNumber = phoneNumber;
+this.next = null;  
+
+}
+newNode.next = head; 
+head = newNode;     
+if (head == null) {
+head = newNode;
+ }
+else {
+ContactNode temp = head;
+while (temp.next != null) {
+temp = temp.next;
+}
+
+ temp.next = newNode;
+}
+
+
+
